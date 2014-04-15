43594813ebc68788a8e6f84758a9edc754dea9b1
Nischal Sheth <nsheth@juniper.net>
2014-04-14 23:11:14 -0700

    Make handling of ShowRouteReq more efficient.

--- src/bgp/routing-instance/routing_instance.h
+++ src/bgp/routing-instance/routing_instance.h
@@ -61,7 +61,7 @@ public:
     void ClearConfig();
 
     static std::string GetTableNameFromVrf(std::string name, Address::Family fmly);
-    static std::string GetVrfFromTableName(const string table);
+    static std::string GetVrfFromTableName(const std::string table);
 
     BgpTable *GetTable(Address::Family fmly);
 
